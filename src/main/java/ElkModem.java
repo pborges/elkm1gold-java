1fa311817b2bef0d3b1a2c01c1e5a1fd9ed001f9
pborges <pborges475@gmail.com>
2015-02-05 19:07:05 -0800

    Move the mapper into an add listener method

--- src/main/java/ElkModem.java
+++ src/main/java/ElkModem.java
@@ -9,7 +9,6 @@ import org.apache.log4j.Logger;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by pborges on 2/5/15.
@@ -20,15 +19,11 @@ public class ElkModem {
     private SerialPort serialPort;
     private ElkRequestEncoder elkRequestEncoder = new ElkRequestEncoder();
     private OutputStreamWriter outputStreamWriter;
-    private HashMap<Class<? extends ElkResponse>, ElkResponseProcessor<? extends ElkResponse>> elkResponseProcessorMap;
+    private HashMap<Class<? extends ElkResponse>, ElkResponseProcessor<? extends ElkResponse>> elkResponseProcessorMap = new HashMap<>();
 
-    public ElkModem(String serialPortName, Integer baud, HashMap<Class<? extends ElkResponse>, ElkResponseProcessor<? extends ElkResponse>> elkResponseProcessorMap) throws PortInUseException, NoSuchPortException, UnsupportedCommOperationException, IOException {
+    public ElkModem(String serialPortName, Integer baud) throws PortInUseException, NoSuchPortException, UnsupportedCommOperationException, IOException {
         log.debug("ElkModem(" + serialPortName + ", " + baud + ")");
-        for (Map.Entry<Class<? extends ElkResponse>, ElkResponseProcessor<? extends ElkResponse>> entry : elkResponseProcessorMap.entrySet()) {
-            log.debug("\tLoading " + entry.getKey().getSimpleName() + " processor " + entry.getValue().getClass().getSimpleName());
-        }
 
-        this.elkResponseProcessorMap = elkResponseProcessorMap;
         CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(serialPortName);
         CommPort commPort = portIdentifier.open(this.getClass().getName(), 2000);
         serialPort = (SerialPort) commPort;
@@ -37,7 +32,13 @@ public class ElkModem {
         outputStreamWriter = new OutputStreamWriter(serialPort.getOutputStream());
     }
 
+    public void addListener(Class<? extends ElkResponse> clazz, ElkResponseProcessor<? extends ElkResponse> processor) {
+        log.debug("Loading " + clazz.getSimpleName() + " processor " + processor.getClass().getSimpleName());
+        elkResponseProcessorMap.put(clazz, processor);
+    }
+
     public void listen() throws IOException {
+
         ElkSerialReader elkSerialReader = new ElkSerialReader(serialPort.getInputStream(), elkResponseProcessorMap);
         new Thread(elkSerialReader).start();
     }
